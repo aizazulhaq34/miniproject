cfb06b24639690f6ca7a609796869e6da17f5358
Aizaz Ul Haq Sultanpori <aizazulhaq34@gmail.com>
2022-03-23 17:55:55 +0530

    changed to email

---
+++ src/app/auth.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from './login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+  
+
+  logout(): void {
+    localStorage.setItem('isLoggedIn', "false");
+    localStorage.removeItem('token');
+  } 
+
+}
---
+++ src/app/login.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core'
+
+
+export class Login{
+
+    constructor(
+
+        // ? is set to define that the arguments marked are optional
+                
+                public username?:string,
+                public password?:string,
+               
+        ){
+        
+    }
+    
+}
