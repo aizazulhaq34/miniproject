347871be882717c85fd568b3f850459d8e1d0d68
Aizaz Ul Haq Sultanpori <aizazulhaq34@gmail.com>
2022-03-23 22:54:46 +0530

    strong password

--- src/app/step/pages/registration.component.ts
+++ src/app/step/pages/registration.component.ts
@@ -9,6 +9,7 @@ import { Registration } from './registration';
 })
 export class RegistrationComponent implements OnInit {
     router: any;
+    passwordpattern:'^(?=(.*[a-z]){3,})(?=(.*[A-Z]){2,})(?=(.*[0-9]){2,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{8,}$';
     ngOnInit() {
 
     }
